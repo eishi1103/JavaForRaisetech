892b0ad3b659d9c658256726389403bc6f574715
eishi1103 <78992733+eishi1103@users.noreply.github.com>
2022-12-22 23:36:36 +0900

    第１回の課題文です
    第１回課題のフォルダの中のsrc内にあるjavaファイルが課題のファイルです。

---
+++ 第１回課題/src/第１回課題/HelloWorld.java
@@ -0,0 +1,15 @@
+package 第１回課題;
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		for(int i=0;i<11;i++) {
+			
+			if(i > 9) {
+				break;
+			}
+			System.out.println("Hello World");
+		}
+		System.out.println("Hello");
+	}
+
+}
